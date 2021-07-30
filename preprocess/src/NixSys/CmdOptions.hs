6b75bfacbf5be3700726e4e1c7d0c6dce68026af
Dmitry Bogatov <git#v1@kaction.cc>
2021-07-30 08:52:41 -0400

    nixsys-preprocess: generate config.h from the manifest
    This is mostly 1-to-1 transctiption of python implementation.

---
+++ preprocess/src/NixSys/CmdOptions.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE TypeFamilies #-}
+
+module NixSys.CmdOptions where
+
+import Options.Applicative
+import System.IO (FilePath)
+
+data CmdOptions = CmdOptions
+  { -- It would be much simpler to just read from stdin, but ghci does not
+    -- support stream redirecting, so I introduce this parameter solely to
+    -- make my development more pleasant. It sucks, I know.
+    manifestFile :: FilePath,
+    -- Path to write config.h
+    outputConfig :: FilePath
+    --     -- Path where database will be finally installed (probably $cdb output)
+    --     finalCDB :: FilePath,
+    --     -- Path to write constant database.
+    --     outputCDB :: f FilePath
+  }
+
+cmdOptions :: Parser CmdOptions
+cmdOptions =
+  CmdOptions
+    <$> strOption
+      ( long "manifest-file"
+          <> metavar "FILE"
+          <> help "source file of manifest in json format"
+          <> value "/dev/stdin"
+          <> showDefault
+      )
+    <*> strOption
+      ( long "output-config"
+          <> metavar "FILE"
+          <> help "path to write output config.h"
+          <> value "/dev/stdout"
+      )
+
+ioCmdOptions :: IO CmdOptions
+ioCmdOptions = execParser opts
+  where
+    opts =
+      info
+        (cmdOptions <**> helper)
+        (fullDesc <> progDesc "preprocess nix-sys manifest")
--- preprocess/src/NixSys/Main.hs
+++ preprocess/src/NixSys/Main.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE GeneralisedNewtypeDeriving #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module NixSys.Main where
@@ -11,17 +12,29 @@ import Control.Monad
 import Data.Aeson
 import Data.Aeson.TH
 import Data.Aeson.Types
+import Data.Coerce (coerce)
+import qualified Data.HashMap.Strict as HashMap
+import Data.List (inits)
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Maybe
+import qualified Data.Set as Set
 import Data.Text (Text)
 import qualified Data.Text as T
+import qualified Data.Text.Lazy.IO as TIO
+import qualified Data.Vector as Vector
 import Data.Word (Word16, Word32)
+import NixSys.CmdOptions
 import Numeric (readOct)
+import System.Exit (exitFailure)
+import Text.Mustache (renderMustache)
+import Text.Mustache.Compile.TH (compileMustacheFile)
 
 -- Absolute file path. We are not performing any operation on it except
 -- validating that it is actually absolute (starts with /).
 newtype Location = Location Text deriving (Eq, Ord, Show)
+
+$(deriveToJSON defaultOptions ''Location)
 
 parseLocation :: Text -> Parser Location
 parseLocation t = do
@@ -41,6 +54,8 @@ instance FromJSONKey Location where
 -- substituting it into template, so we do not use more type-safe
 -- representation.
 newtype Mode = Mode Word16 deriving (Show)
+
+$(deriveToJSON defaultOptions ''Mode)
 
 instance FromJSON Mode where
   parseJSON e = parseText e <|> parseInt e
@@ -67,6 +82,8 @@ data SpecCopy = SpecCopy
   }
   deriving (Show)
 
+$(deriveToJSON defaultOptions ''SpecCopy)
+
 data SpecMkdir = SpecMkdir
   { mode :: Mode,
     owner :: Word32,
@@ -74,8 +91,16 @@ data SpecMkdir = SpecMkdir
   }
   deriving (Show)
 
+$(deriveToJSON defaultOptions ''SpecMkdir)
+
 newtype SpecSymlink = SpecSymlink {path :: Location}
-  deriving (Show, FromJSON)
+  deriving (Show)
+
+instance FromJSON SpecSymlink where
+  parseJSON = withObject "SpecSymlink" $ \o ->
+    SpecSymlink <$> ((o .: "path") >>= parseLocation)
+
+$(deriveToJSON defaultOptions ''SpecSymlink)
 
 data Spec = Spec
   { copy :: Map Location SpecCopy,
@@ -112,7 +137,59 @@ instance FromJSON Spec where
       <*> fmap (fromMaybe Map.empty) (o .:? "symlink")
       <*> o .:? "exec"
 
-main =
-  eitherDecodeFileStrict "input.json" >>= \case
-    Left err -> putStrLn err
-    Right a -> print (a :: Spec)
+-- Return sorted list of parent directories of spec targets, so nix-sys
+-- do not need to do string manipulation in C.
+parents :: Spec -> [Text]
+parents s =
+  let locations =
+        coerce $
+          Map.keys (copy s)
+            ++ Map.keys (mkdir s)
+            ++ Map.keys (symlink s)
+      parents1 :: Text -> [Text]
+      parents1 = map (T.intercalate "/") . inits . T.splitOn "/"
+
+      sortAsc :: Ord a => [a] -> [a]
+      sortAsc = Set.toList . Set.fromList
+   in sortAsc . filter (/= "") . join . map parents1 $ locations
+
+-- Mustache does not support iterating over dictionary keys, only over
+-- list elements, but 'Spec' datatype uses dictionary instead of list to
+-- make some incoherent manifests unrepresentable.
+--
+-- So here we convert it dictionary with variable keys into the list.
+-- This function uses partial functions to avoid a lot of boilerplate,
+-- but is total by itself.
+specToContext :: Spec -> Text -> Value
+specToContext s hash =
+  object
+    [ ("copy", fromMap (copy s)),
+      ("mkdir", fromMap (mkdir s)),
+      ("symlink", fromMap (symlink s)),
+      ("exec", maybe Null locToString (exec s)),
+      ("parents", Array . Vector.fromList . map String . parents $ s)
+    ]
+  where
+    locToString :: Location -> Value
+    locToString (Location s) = String s
+
+    fromMap :: (ToJSON a) => Map Location a -> Value
+    fromMap = Array . Vector.fromList . map f . Map.toList
+
+    f :: ToJSON a => (Location, a) -> Value
+    f (Location target, a) =
+      let Object m = toJSON a -- partial
+       in Object
+            . HashMap.insert "target" (String target)
+            . HashMap.insert "hash" (String hash)
+            $ m
+
+main = do
+  CmdOptions {..} <- ioCmdOptions
+  spec <-
+    eitherDecodeFileStrict manifestFile >>= \case
+      Left err -> putStrLn err >> exitFailure
+      Right a -> pure a
+  let template = $(compileMustacheFile "./data/config.h.mustache")
+  TIO.writeFile outputConfig $
+    renderMustache template (specToContext spec "foo")
