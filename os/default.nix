b67683dc5b494fa54632fa155433aa90d1688f32
Dmitry Bogatov <git#v1@kaction.cc>
2021-09-26 17:03:16 -0400

    Make it possible to choose system config in lilo

--- os/default.nix
+++ os/default.nix
@@ -1,5 +1,7 @@
-{ nixsys, writeText, callPackage, pkgs }:
+{ nixsys, writeText, runCommand, callPackage, pkgs }:
 let
+  kernel = callPackage ./linux { };
+  kernel-sha256 = builtins.substring 11 32 kernel;
   service =
     # logscript has default, since in most cases redirecting stdout/stderr
     # is disirable, otherwise /dev/tty1 will be cluttered.
@@ -37,10 +39,6 @@ let
         path = callPackage ./path/v2/libexec/init { };
         mode = "555";
       };
-      "/boot/kernel" = {
-        path = callPackage ./linux { };
-        mode = "444";
-      };
     };
     symlink = {
       "/service/getty-tty1" = {
@@ -77,6 +75,9 @@ let
       };
     };
     mkdir = {
+      "/boot/kernel" = { mode = "755"; };
+      "/boot/kernel/hash" = { mode = "755"; };
+      "/boot/kernel/conf" = { mode = "755"; };
       "/state/supervise" = { mode = "700"; };
       "/state/log/sshd" = { mode = "700"; };
       "/state/log/net-eth0" = { mode = "700"; };
@@ -84,14 +85,86 @@ let
       "/state/log/getty-tty1" = { mode = "700"; };
       "/state/identity/sshd" = { mode = "700"; };
     };
-    exec = let path = pkgs.lib.makeBinPath (with pkgs; [ busybox dropbear ]);
-    in pkgs.writeScript "post-install" ''
-      #!${pkgs.busybox}/bin/sh -eu
-      export PATH=${path}
-      if ! [ -f /state/identity/sshd/ed25519 ] ; then
-        dropbearkey -t ed25519 -f /state/identity/sshd/ed25519
-      fi
-    '';
+    exec = let
+      path = with pkgs; lib.makeBinPath [ nix tinycdb busybox dropbear lilo ];
+    in pkgs.writeScript "post-install" (
+      # ssh server key can't be generated at build time, or it will be the same
+      # at all targets
+      ''
+        #!${pkgs.busybox}/bin/sh -eu
+        umask 022
+        export PATH=${path}
+        if ! [ -f /state/identity/sshd/ed25519 ] ; then
+          dropbearkey -t ed25519 -f /state/identity/sshd/ed25519
+        fi
+      ''
+      # By design, nix-sys removes files from previous config. We don't want
+      # it to happen with kernels, so this have to be done imperatively in
+      # post-install script.
+      + ''
+        epoch=`date +%s`
+        now=`date -d @$epoch +%Y-%m-%d.%s`
+      ''
+      # LILO has very small limit on length of label, so we barely can fit only
+      # timestamp, nothing more.
+      + ''
+        label=`date -d @$epoch +%Y%m%d%H%S`
+        if ! test -f /boot/kernel/hash/${kernel-sha256} ; then
+          cp ${kernel} /boot/kernel/hash/${kernel-sha256}
+        fi
+      ''
+      # Storing kernel by its hash (well, output hash) and hard-linking kernel
+      # afterward saves storage on /boot partition which is usually quite small
+      # (e.g Alpine linux dedicates merely 90Mb to it).
+      #
+      # Now, for every new invocation of nix-sys, we create new entry in
+      # bootloader, while taking care that two consequent calls to nix-sys
+      # (with same output hash) do not create duplicate entries.
+      + ''
+        current="none"
+        ! test -r /boot/current || current=`cat /boot/current`
+
+        if [ $out != $current ] ; then
+          this=`echo $out | cut -b12-43`
+          mkdir -p /boot/kernel/conf/$now
+          ln /boot/kernel/hash/${kernel-sha256} /boot/kernel/conf/$now/image
+          ln -sf $out /boot/kernel/conf/$now/nix-sys.gc
+          nix-store --add-root /boot/kernel/conf/$now/nix-sys.gc -r
+          cat << EOF > /boot/kernel/conf/$now/lilo.conf
+        image = /boot/kernel/conf/$now/image
+          label  = "$label"
+          append = "init=/v2/libexec/init"
+          read-only
+        EOF
+
+        # Regenerate /boot/lilo.conf
+          cat << EOF > /boot/lilo.conf~
+        lba32
+        boot = /dev/sda
+        root = /dev/sda3
+        install = menu
+        prompt
+        timeout = 100
+        EOF
+
+          for x in `ls /boot/kernel/conf/*/lilo.conf | tac` ; do
+            cat "$x" >> /boot/lilo.conf~
+          done
+
+          cat << EOF >> /boot/lilo.conf~
+        image = /boot/bzImage-2
+          label = "Linux-2"
+          read-only
+
+        image = /boot/bzImage-1
+          label = "Linux-1"
+          read-only
+        EOF
+
+         mv /boot/lilo.conf~ /boot/lilo.conf
+         lilo -C /boot/lilo.conf
+        fi
+      '');
   };
 in nixsys.override {
   manifest = writeText "manifest.json" (builtins.toJSON manifest);
